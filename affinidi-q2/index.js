76d0a0147569d70b5dbb3f3cdb5c549cb1ccdac1
ALIPHATIC <adegokefuad1@gmail.com>
2024-01-29 05:13:11 +0100

    Refactor express application and integrate Affinidi Login

--- affinidi-q2/index.js
+++ affinidi-q2/index.js
@@ -6,13 +6,15 @@ const { Issuer, Strategy } = require('openid-client');
 const http = require("http");
 require("dotenv").config();
 
-//  2. Create an express application and set up session
+//  2. Create an express application and setup session
 const app = express();
 app.use(session({
     secret: 'my-secret',
     resave: false ,
     saveUninitialized: false
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // 3. Define application end-points
 app.get("/", (req, res) => {
@@ -23,13 +25,16 @@ app.get('/login',
     function (req, res, next) {
         next();
     },
+    passport.authenticate('affinidi-login',{scope:'openid'})
 );
 
-app.get('/login/callback', (req, res, next) => {})
+app.get('/login/callback', (req, res, next) => {
+    passport.authenticate('affinidi-login', {successRedirect: '/protected', failureRedirect: '/'})(req,res,next)
+})
 
 app.get("/protected", (req, res) => {
     res.header("Content-Type", 'application/json');
-    res.end("Protected Content for Authenticated Users");
+    res.end(JSON.stringify(req.user, null, 4));
 })
 
 // 4. Start the http server
@@ -37,3 +42,31 @@ const httpServer = http.createServer(app)
 httpServer.listen(8080, () => {
     console.log(`Hello World - Affinidi Login : Up and running on 8080`)
 })
+
+// 5. Integrate Authentication
+// 5a. Discover Affinidi Login - Issuer
+Issuer.discover(process.env.issuer).then(function (oidcIssuer) {
+    // 5b. Create a RP-client which can initiate an OIDC flow
+    var client = new oidcIssuer.Client({
+      client_id: process.env.client_id,
+      client_secret: process.env.client_secret,
+      redirect_uris: ["http://localhost:8080/login/callback"],
+      response_types: ['code'],
+      token_endpoint_auth_method: 'client_secret_post'
+    });
+
+    // 5c. Provide this strategy to the passport middleware
+    passport.use(
+      'affinidi-login', new Strategy({ client, passReqToCallback: true }, (req, tokenSet, userinfo, done) => {
+        req.session.tokenSet = tokenSet;
+        req.session.userinfo = userinfo;
+        return done(null, tokenSet.claims());
+      }));
+  });
+
+  passport.serializeUser(function (user, done) {
+    done(null, user);
+  });
+  passport.deserializeUser(function (user, done) {
+    done(null, user);
+  });
